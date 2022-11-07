41659c5a4903fd00aeed312ec49cb209d3ef5f66
DongEEun <91827896+kimdongeun12@users.noreply.github.com>
2022-11-08 02:43:18 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,3 @@
 function solution(s) {
-    let answer = '';
-    if(parseInt(s.length/2) !== s.length/2){
-        answer = s[parseInt(s.length/2)]
-    }else{
-        answer = s[parseInt(s.length/2) - 1] + s[parseInt(s.length/2)] 
-    }
-    return answer;
+    return parseInt(s.length/2) !== s.length/2 ? s[parseInt(s.length/2)] : s[parseInt(s.length/2) - 1] + s[parseInt(s.length/2)];;
 }
