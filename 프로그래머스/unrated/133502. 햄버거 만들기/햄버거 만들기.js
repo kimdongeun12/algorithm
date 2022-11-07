eab56f93dab7d83ea631ea13d28cde5a2c38dc5b
DongEEun <91827896+kimdongeun12@users.noreply.github.com>
2022-11-08 01:32:07 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,20 @@
+function solution(ingredient) {
+    let answer = 0;
+    const arr = [];
+  ingredient.forEach((cur) => {
+    arr.push(cur);
+    if(arr[arr.length - 1] === 1 && 
+       arr[arr.length - 2] === 3 && 
+       arr[arr.length - 3] === 2 && 
+       arr[arr.length - 4] === 1) {
+        
+      arr.pop()
+      arr.pop()
+      arr.pop()
+      arr.pop()
+      answer++
+    }
+  });
+
+  return answer
+}
