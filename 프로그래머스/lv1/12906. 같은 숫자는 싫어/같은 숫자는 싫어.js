35e9ff4deb66e3738a6d32212b46cf2e170b0aea
DongEEun <91827896+kimdongeun12@users.noreply.github.com>
2022-11-07 17:46:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.54 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,17 @@
 function solution(arr)
 {
-    let answer = [];
-    let oldNum = null
-    arr.map((cur ) => {
-        if(oldNum !== cur){
-            answer.push(cur)
-        }
-        oldNum = cur
-    })
+    const answer = [];
+//     let oldNum = null
+//     arr.map((cur ) => {
+//         if(oldNum !== cur){
+//             answer.push(cur)
+//         }
+//         oldNum = cur
+//     })
+    
+    for(let i=0; i< arr.length; i++){
+        if(arr[i] !== arr[i+1]) answer.push(arr[i])
+    }
     
     return answer;
 }
