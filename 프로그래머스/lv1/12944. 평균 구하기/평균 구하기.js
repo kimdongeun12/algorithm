60461dbc60e04c3423211a785b1085a50aefe7a8
DongEEun <91827896+kimdongeun12@users.noreply.github.com>
2022-11-08 17:40:16 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,8 +1,6 @@
 function solution(arr) {
-    let answer = 0;
     let sum = arr.reduce((acc , cur) => {
         return acc + cur;
     })
-    answer = sum / arr.length
-    return answer;
+    return sum / arr.length;
 }
