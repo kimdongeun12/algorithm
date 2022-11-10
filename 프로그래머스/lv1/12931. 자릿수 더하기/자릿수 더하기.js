7e98ebb9e2e1cbf93f704344cf3da35c7210d889
DongEEun <91827896+kimdongeun12@users.noreply.github.com>
2022-11-10 17:39:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,7 @@
 function solution(n){
-    const answer = String(n).split('').reduce((acc , cur) => {
-       return Number(acc) + Number(cur)
+    const answer = String(n).split('')
+    .reduce((acc , cur) => {
+       return acc + Number(cur)
     },0);
 
     return answer;
