2c8346116cc8fbc17969f314dd526656c49d3ba1
DongEEun <kdedong12@gmail.com>
2022-12-13 17:46:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,4 +1,19 @@
 function solution(n) {
-    const three = n.toString(3).split('').reverse().join('');
-    return Number.parseInt(three,3);
+    // const three = n.toString(3).split('').reverse().join('');
+    // return Number.parseInt(three,3);
+    
+    // // 1. 3진법으로 변환
+    // n = n.toString(3)
+    // // 2. 앞뒤 반전(뒤집기)
+    // let reverse = '';
+    // for(let i = n.length-1; i >=0; i--){
+    //     reverse += n[i]
+    // }
+    
+    n = n.toString(3)
+         .split('')
+         .reverse()
+         .join('')
+    
+    return parseInt(n,3)
 }
