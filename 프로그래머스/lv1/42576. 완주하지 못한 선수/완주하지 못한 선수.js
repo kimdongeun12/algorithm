373ade9b390536bbf958d78030a631b62b87d226
DongEEun <kdedong12@gmail.com>
2023-04-09 16:34:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.82 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,7 @@
 function solution(participant, completion) {
-    var answer = '';
-    var a = participant.sort()
-    var b = completion.sort()
-    for(var i = 0; i<= a.length; i++)
-    if(a[i] !== b[i]){
-        answer = a[i]
-        break
+    participant.sort()
+    completion.sort()
+    for(let i = 0; i<= participant.length; i++){
+      if(participant[i] !== completion[i]) return participant[i]
     }
-    
-    return answer;
 }
