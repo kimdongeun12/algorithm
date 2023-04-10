aabee3a42c40367ac371be8e00a3465af95a43b8
DongEEun <kdedong12@gmail.com>
2023-04-10 15:06:19 +0900

    [level 3] Title: 베스트앨범, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -29,24 +29,9 @@ function solution(genres, plays) {
         return a[a.length - 1] > b[b.length - 1] && -1;
     });
     arr.forEach((firstEl) => {
-        // console.log(firstEl);
         firstEl.forEach((secEl) => {
             if(isNaN(secEl)) answer.push(secEl.idx);
         });
     });
     return answer;
 }
-
-// genres : ["classic", "pop", "classic", "classic", "pop"]
-// plays : [800, 600, 150, 800, 2500]
-// answer : [4, 1, 0, 3]
-
-// 위 케이스를 고려하지 않으면 [4, 1, 3, 0]이 출력 됩니다.
-
-
-// 총 플레이 수를 합산된 값이 더 큰 장르가 먼저 앞에 오도록 해야 합니다.
-// 아래 테스트 케이스에서 [4, 3, 1, 0]이 나온다면 코드를 수정해야 할 것 같네요
-
-// ["pop", "pop", "pop", "rap", "rap"]
-// [45, 50, 40, 60, 70]
-// 답: [1, 0, 4, 3]
